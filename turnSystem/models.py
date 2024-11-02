931c1552c15eb12d827932f6e0121d0a94b653cd
Dax <daxnavarrete@gmail.com>
2024-11-02 12:54:52 -0500

    last turnSystem logic closing support to the project

--- turnSystem/models.py
+++ turnSystem/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Turn(models.Model):
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    atendido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Turno de {self.usuario.username} - {'Atendido' if self.atendido else 'Pendiente'}"
--- turnSystem/views.py
+++ turnSystem/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Turn
+from .serializers import TurnSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class AsignarTurnView(generics.CreateAPIView):
+    serializer_class = TurnSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        usuarios = User.objects.all()
+        ultimo_turn = Turn.objects.last()
+        if ultimo_turn:
+            ultimo_usuario = ultimo_turn.usuario
+            siguiente_usuario = usuarios[(list(usuarios).index(
+                ultimo_usuario) + 1) % len(usuarios)]
+        else:
+            siguiente_usuario = usuarios.first()
+
+        turn = Turn.objects.create(usuario=siguiente_usuario)
+        serializer = self.get_serializer(turn)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ListaTurnsView(generics.ListAPIView):
+    queryset = Turn.objects.all()
+    serializer_class = TurnSerializer
+    permission_classes = [IsAuthenticated]
